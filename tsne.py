eb7041067001d1c00c16c6139f88b233bdf7ce87
MUNEEB T  H <muneebth@gmail.com>
2015-07-02 23:45:24 +0530

    Update tsne.py

--- tsne.py
+++ tsne.py
@@ -1,14 +1,4 @@
-#
-#  tsne.py
-#  
-# Implementation of t-SNE in Python. The implementation was tested on Python 2.5.1, and it requires a working 
-# installation of NumPy. The implementation comes with an example on the MNIST dataset. In order to plot the
-# results of this example, a working installation of matplotlib is required.
-# The example can be run by executing: ipython tsne.py -pylab
-#
-#
-#  Created by Laurens van der Maaten on 20-12-08.
-#  Copyright (c) 2008 Tilburg University. All rights reserved.
+
 
 import numpy as Math
 import pylab as Plot
